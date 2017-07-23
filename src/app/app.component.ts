d6def9d1d9fc124566d79570185ec65b666ad6be
Jerry Lin <jerry.jzlin@gmail.com>
2017-07-23 10:36:33 +0800

    任務 06：套用事件繫結 (Event Binding)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   inputHint = 'What needs to be done?';
+  todos: any[] = [];
+
+  addTodo(event: KeyboardEvent) {
+    console.log(event);
+    if (event.keyCode === 13) {
+      console.log('enter');
+      let inputElem = <HTMLInputElement>event.target;
+      this.todos.push(inputElem.value);
+    }
+  }
 
 }
