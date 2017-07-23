0b64c30a906883659ceb4387615d9d59a9b30980
Jerry Lin <jerry.jzlin@gmail.com>
2017-07-23 15:20:07 +0800

    任務 15：使用 Http 服務元件呼叫 RESTful APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from "@angular/http";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   inputHint = 'What needs to be done?';
   todos: any[] = [];
   todo: string = '';
   filterType: string = 'All';
   isToggleAll: boolean = false;
+  apiUrl = 'http://localhost:3000/todos';
 
   constructor(private http: Http) {}
 
+  ngOnInit() {
+    this.getTodosFromApi();
+  }
+
+  private getTodosFromApi() {
+    this.http.get(this.apiUrl).subscribe(res => {
+      this.todos = res.json();
+    });
+  }
+
   addTodo() {
     if (this.todo) {
-      this.todos = [...this.todos, {
+      this.http.post(this.apiUrl, {
         text: this.todo,
         done: false
-      }];
-      this.todo = '';
+      }).subscribe(res => {
+        this.todos = [...this.todos, res.json()];
+        this.todo = '';
+      });
     }
   }
 
@@ -31,7 +45,11 @@ export class AppComponent {
   }
 
   clearCompleted() {
-    this.todos = this.todos.filter(item => !item.done);
+    this.todos
+      .filter(item => item.done)
+      .forEach(item => {
+        this.removeTodo(item);
+      });
   }
 
   filterTodos(event) {
@@ -39,14 +57,28 @@ export class AppComponent {
   }
 
   toggleAll(event) {
-    this.todos = this.todos.map(item => {
-      item.done = event;
-      return item;
+    Observable.forkJoin(
+      this.todos
+        .map(item => {
+          item.done = event;
+          return item;
+        })
+        .map(item => {
+          return this.http.put(`${this.apiUrl}/${item.id}`, item);
+        })
+    ).subscribe(res => {
+      this.getTodosFromApi();
     });
   }
 
+  updateTodo(todo) {
+    this.http.put(`${this.apiUrl}/${todo.id}`, todo).subscribe();
+  }
+
   removeTodo(todo) {
-    this.todos = this.todos.filter(item => item != todo);
+    this.http.delete(`${this.apiUrl}/${todo.id}`).subscribe(res => {
+      this.todos = this.todos.filter(item => item != todo);
+    });
   }
 
 }
